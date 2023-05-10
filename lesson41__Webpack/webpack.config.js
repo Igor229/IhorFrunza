d48896fa642b71fe8105ff16befb7adf069ee706
Ihor <frunza2029@gmail.com>
2023-05-10 11:23:54 +0300

    working with webpack, webpack serve && sass loader

---
+++ lesson41__Webpack/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('node:path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        })
+    ],
+    devServer: {
+        port: 3000,
+    },
+    module: {
+        rules: [
+          {
+            test: /\.s[ac]ss$/i,
+            use: [
+              // Creates `style` nodes from JS strings
+              "style-loader",
+              // Translates CSS into CommonJS
+              "css-loader",
+              // Compiles Sass to CSS
+              "sass-loader",
+            ],
+          },
+        ],
+      },
+};
