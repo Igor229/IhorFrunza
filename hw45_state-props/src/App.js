b00030d341dec7ec8f66804f7a096798cd601d72
Ihor <frunza2029@gmail.com>
2023-05-18 16:42:42 +0300

    creating Blog template with react components (home-work middle-level)

--- hw45_state-props/src/App.js
+++ hw45_state-props/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import "./style.scss";
+import Header from './Header';
+import Banner from './Banner';
+import Blog from './Blog';
+import News from './News';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header title = 'Blog name' />,
+      <Banner bannerTitle = 'Full-width banner image'/>
+
+      <main className='main-content'>
+        <Blog/>
+        <News/>
+      </main>
+    </>
+  )
 }
 
 export default App;
---
+++ hw45_state-props/src/Blog.js
@@ -0,0 +1,16 @@
+function Blog() {
+    return (
+        <section className="main-content__item blog">
+            <h3 className="main-content__item-title">Blog</h3>
+          
+            <div className="blog__items">
+                <div className="blog__items-item"></div>
+                <div className="blog__items-item"></div>
+                <div className="blog__items-item"></div>
+                <div className="blog__items-item"></div>
+            </div>
+        </section>
+    )
+}
+
+export default Blog;
